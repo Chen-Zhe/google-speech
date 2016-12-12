9790780f3b3e5e8c682e1b579d73b912983bf260
Chen Zhe <@XPS15>
2016-12-13 00:22:38 +0800

    in progress

--- google-speech/main.cpp
+++ google-speech/main.cpp
@@ -1,15 +1,49 @@
 #include "base64.h"
 #include <iostream>
+#include <fstream>
+using namespace std;
 
 int main() {
-	const std::string s = "ADP GmbH\nAnalyse Design";
+	//read
+	std::ifstream file("testing-voice.pcm", ifstream::binary);
+	int contentSize = 20480;//bytes
+	char* fileContent = (char*)malloc(contentSize+44);
 
-	std::string encoded = base64_encode(reinterpret_cast<const unsigned char*>(s.c_str()), s.length());
-	std::string decoded = base64_decode(encoded);
+	file.seekg(0, ifstream::beg);
+	file.read(fileContent+44, contentSize);
+	file.close();
 
-	std::cout << "encoded: " << encoded << std::endl;
-	std::cout << "decoded: " << decoded << std::endl;
+	struct waveHeader{
+		// RIFF header.		
+		char riff[4] = {'R', 'I', 'F', 'F'};// Chunk ID.		
+		uint32_t riffSize;// Chunk size.		
+		char wave[4] = { 'W', 'A', 'V', 'E' };// Format.
 
-	getchar();
-	return 0;
+		// fmt block
+		char fmt[4] = { 'f', 'm', 't', ' ' };
+		uint32_t fmtSize = 16;// fmt block size: 16 - normal, 18 - extra data		
+		
+		//fmt block wave format definition
+		uint16_t formatTag = 1;// Audio format (floating point (3) or PCM (1)). Any other format indicates compression.
+		uint16_t numChannels = 1;// Channels.		
+		uint32_t samplingRate = 16000;// Sampling rate.		
+		uint32_t avgByteRate = 32000;// Average bytes per second =sampleRate * channelCount * (bitDepth / 8)
+		uint16_t blockAlign = 2; //channelCount * (bitDepth / 8)
+		uint16_t bitDepth = 16; // Bits per sample;
+
+		// data block		
+		char data[4] = { 'd', 'a', 't', 'a' };
+		uint32_t dataSize;// data size.
+
+	};
+	waveHeader* ptr =(waveHeader*) fileContent;
+	ptr->riffSize = contentSize + 36;
+	ptr->dataSize = contentSize;
+
+	//std::string encoded = base64_encode(reinterpret_cast<const unsigned char*>(fileContent), fileSize);
+	std::ofstream outfile("voice.wav", ofstream::binary);
+	cout << "writing..." << endl;
+	outfile.write((const char*)fileContent, contentSize);
+	outfile.close();
+
 }
---
+++ google-speech/sprec/sprec.h
@@ -0,0 +1,10 @@
+#ifndef __SPREC_SPREC_H__
+#define __SPREC_SPREC_H__
+
+#include <sprec/wav.h>
+#include <sprec/flac_encoder.h>
+#include <sprec/web_client.h>
+#include <sprec/recognize.h>
+
+#endif /* !__SPREC_SPREC_H__ */
+
