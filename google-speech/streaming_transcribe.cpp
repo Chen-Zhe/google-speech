269e2cdb2a1e16d9a22612ff75783681d98b18d2
chenzhe <chenzhe@MML-PC23>
2016-12-22 15:12:05 +0800

    rewrite sample for testing

--- google-speech/streaming_transcribe.cpp
+++ google-speech/streaming_transcribe.cpp
@@ -34,29 +34,27 @@ RecognitionDataStreamer streamer;
 // Write the audio in 64k chunks at a time, simulating audio content arriving
 // from a microphone.
 
-void *voiceActivityDetector(void *null) {
-  StreamingRecognizeRequest request;
-  std::ifstream file_stream("brown-fox.pcm");
-  const size_t chunk_size = 20 * 1024;
-  std::vector<char> chunk(chunk_size);
+void *StreamingSpeechRecognition(void *null) {
 
-  while (true) {
-    // Read another chunk from the file.
-    std::streamsize bytes_read =
-        file_stream.rdbuf()->sgetn(&chunk[0], chunk.size());
-    // And write the chunk to the stream.
-    request.set_audio_content(&chunk[0], bytes_read);
-    std::cout << "Sending " << bytes_read / 1024 << "k bytes." << std::endl;
-    streamer->Write(request);
-    if (bytes_read < chunk.size()) {
-      // Done reading everything from the file, so done writing to the stream.
-      streamer->WritesDone();
-      break;
-    } else {
-      // Wait a second before writing the next chunk.
-		sleep(1);
-    }
-  }
+
+	StreamingRecognizeResponse response;
+	while (streamer->Read(&response)) {  // Returns false when no more to read.
+										 // Dump the transcript of all the results.
+		std::cout << "response read" << std::endl;
+		for (int r = 0; r < response.results_size(); ++r) {
+			auto result = response.results(r);
+			std::cout << "Result stability: " << result.stability() << std::endl;
+			for (int a = 0; a < result.alternatives_size(); ++a) {
+				auto alternative = result.alternatives(a);
+				std::cout << alternative.confidence() << "\t"
+					<< alternative.transcript() << std::endl;
+			}
+		}
+	}
+
+
+
+  
  
 }
 
@@ -75,31 +73,41 @@ int main(int argc, char** argv) {
   streaming_config->mutable_config()->set_sample_rate(16000);
   streaming_config->mutable_config()->set_encoding(RecognitionConfig::LINEAR16);
   streaming_config->mutable_config()->set_language_code("en-US");
+  streaming_config->set_interim_results(true);
 
   // Begin a stream.
-  grpc::ClientContext context;
-  streamer = speech->StreamingRecognize(&context);
+  grpc::ClientContext *context = new grpc::ClientContext;
+  streamer = speech->StreamingRecognize(context);
   // Write the first request, containing the config only.
-  streaming_config->set_interim_results(true);
+  
   streamer->Write(request);
   // The microphone thread writes the audio content.
   pthread_t microphone;
-  pthread_create(&microphone, NULL, voiceActivityDetector, (void *)NULL);
+  pthread_create(&microphone, NULL, StreamingSpeechRecognition, (void *)NULL);
   // Read responses.
-  StreamingRecognizeResponse response;
-  while (streamer->Read(&response)) {  // Returns false when no more to read.
-    // Dump the transcript of all the results.
-	  std::cout << "response read" << std::endl;
-    for (int r = 0; r < response.results_size(); ++r) {
-      auto result = response.results(r);
-      std::cout << "Result stability: " << result.stability() << std::endl;
-      for (int a = 0; a < result.alternatives_size(); ++a) {
-        auto alternative = result.alternatives(a);
-        std::cout << alternative.confidence() << "\t"
-                  << alternative.transcript() << std::endl;
-      }
-    }
+  StreamingRecognizeRequest streamReq;
+  std::ifstream file_stream("brown-fox.pcm");
+  const size_t chunk_size = 20 * 1024;
+  std::vector<char> chunk(chunk_size);
+  while (true) {
+	  // Read another chunk from the file.
+	  std::streamsize bytes_read =
+		  file_stream.rdbuf()->sgetn(&chunk[0], chunk.size());
+	  // And write the chunk to the stream.
+	  streamReq.set_audio_content(&chunk[0], bytes_read);
+	  std::cout << "Sending " << bytes_read / 1024 << "k bytes." << std::endl;
+	  streamer->Write(streamReq);
+	  if (bytes_read < chunk.size()) {
+		  // Done reading everything from the file, so done writing to the stream.
+		  streamer->WritesDone();
+		  break;
+	  }
+	  else {
+		  // Wait a second before writing the next chunk.
+		  sleep(1);
+	  }
   }
+  
   grpc::Status status = streamer->Finish();
   pthread_join(microphone, NULL);
   if (!status.ok()) {
